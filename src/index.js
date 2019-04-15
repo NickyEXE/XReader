6411a522987223a0de535033c7a1633bd64e686f
NickyEXE <dovern42@gmail.com>
2019-04-15 12:08:03 -0400

    added collision physics

--- src/index.js
+++ src/index.js
@@ -6,17 +6,39 @@ function domLoadFunctions(){
   const ctx = canvas.getContext('2d');
   createTheBox()
   const avatar = {x: 0, y: Math.round(canvas.height/2), height: Math.round(canvas.height/15), width: Math.round(canvas.height/15)}
-  renderer(avatar)
+  const words = [{word: "Ponies", x: canvas.width-100, y: canvas.height/2}]
+  renderer()
   document.addEventListener("keydown", keydownHandler)
+  setInterval(wordsLogic, 10);
+
+  function wordsLogic(){
+    words.forEach(word => wordLogic(word))
+    renderer()
+  }
+
+  function wordLogic(word){
+    word.x = word.x - 2
+    // if the front or back collides on x
+    const areWeCollidingOnX = (avatar.x > word.x && avatar.x < word.x + 100) || (avatar.x+avatar.width > word.x && avatar.x+avatar.width < word.x + 100)
+    const areWeCollidingOnY = (avatar.y > word.y && avatar.y < word.y + 100) || (avatar.y+avatar.height > word.y && avatar.y+avatar.height < word.y + 100)
+    if (areWeCollidingOnX && areWeCollidingOnY){
+      alert("game over")
+      document.body.innerHTML = ""
+      avatar.x = 9999999
+      avatar.y = 9999999
+    }
+  }
 
   function renderer(objs){
     createTheBox()
     if (avatar.y > canvas.height - avatar.height){avatar.y=canvas.height - avatar.height}
     if (avatar.y < 0){avatar.y = 0}
     printTheAvatar(avatar)
-    console.log("canvas",canvas.height)
-    console.log("dodger x", avatar.x)
-    console.log("dodger y", avatar.y)
+    words.forEach(word => renderWord(word))
+  }
+
+  function renderWord(word){
+    ctx.fillRect(word.x, word.y, 100, 100);
   }
 
   function createTheBox()
@@ -42,9 +64,6 @@ function domLoadFunctions(){
 
 
   function printTheAvatar(avatar){
-    console.log("canvas",canvas.height)
-    console.log("dodger x", avatar.x)
-    console.log("dodger y", avatar.y)
     ctx.fillRect(avatar.x, avatar.y, avatar.width, avatar.height);
   }
 
