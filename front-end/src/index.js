639e08be8774cff970760937024337efd7460e20
NickyEXE <dovern42@gmail.com>
2019-04-15 13:49:22 -0400

    all broke again

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,10 +6,15 @@ function domLoadFunctions(){
   const ctx = canvas.getContext('2d');
   createTheBox()
   const avatar = {x: 0, y: Math.round(canvas.height/2), height: Math.round(canvas.height/15), width: Math.round(canvas.height/15)}
-  const words = [{word: "Ponies", x: canvas.width-100, y: canvas.height/2}]
+  const words = [{word: "Ponies", x: canvas.width-100, y: canvas.height/2, width: 0, height: 16}]
   renderer()
   document.addEventListener("keydown", keydownHandler)
   setInterval(wordsLogic, 10);
+  setInterval(createWords, 3000)
+
+  function createWords(){
+    words.push({word: "Squirrel", x: canvas.width-100, y: Math.floor(Math.random() * (canvas.height-16)) + 1, width: 0, height: 16})
+  }
 
   function wordsLogic(){
     words.forEach(word => wordLogic(word))
@@ -19,14 +24,23 @@ function domLoadFunctions(){
   function wordLogic(word){
     word.x = word.x - 2
     // if the front or back collides on x
-    const areWeCollidingOnX = (avatar.x > word.x && avatar.x < word.x + 100) || (avatar.x+avatar.width > word.x && avatar.x+avatar.width < word.x + 100)
-    const areWeCollidingOnY = (avatar.y > word.y && avatar.y < word.y + 100) || (avatar.y+avatar.height > word.y && avatar.y+avatar.height < word.y + 100)
+    const isAvatarLeftSideLefterThanLeftSideOfWord = avatar.x > word.x
+    const isAvatarLeftSideRighterThanRightSideOfWord = avatar.x < word.x + word.width
+    const isAvatarRightSideRighterThanLeftSideOfWord = avatar.x+avatar.width > word.x
+    const isAvatarRightSideLefterThanRightSideOfWord = avatar.x+avatar.width < word.x + word.width
+    const isAvatarBottomGreater
+    const areWeCollidingOnY = (avatar.y > word.y && avatar.y < word.y + word.height) ||
+    (avatar.y+avatar.height > word.y && avatar.y+avatar.height < word.y + word.height)  if (word.x<100){
+    console.log(word.x, avatar.x, areWeCollidingOnX)
+    console.log(word.y, avatar.y, areWeCollidingOnY)}
     if (areWeCollidingOnX && areWeCollidingOnY){
       alert("game over")
       document.body.innerHTML = ""
       avatar.x = 9999999
       avatar.y = 9999999
     }
+    if (word.x < -40){words.shift()}
+
   }
 
   function renderer(objs){
@@ -38,7 +52,9 @@ function domLoadFunctions(){
   }
 
   function renderWord(word){
-    ctx.fillRect(word.x, word.y, 100, 100);
+    ctx.font = "16px Arial";
+    ctx.fillText(word.word, word.x, word.y);
+    word.width = Math.ceil(ctx.measureText(word.word).width)
   }
 
   function createTheBox()
