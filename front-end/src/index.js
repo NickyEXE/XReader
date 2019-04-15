78122c1e661ec25303d2eb797459efe5ad8d5194
NickyEXE <dovern42@gmail.com>
2019-04-15 14:59:53 -0400

    we collide

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,11 +6,11 @@ function domLoadFunctions(){
   const ctx = canvas.getContext('2d');
   createTheBox()
   const avatar = {x: 0, y: Math.round(canvas.height/2), height: Math.round(canvas.height/15), width: Math.round(canvas.height/15)}
-  const words = [{word: "Ponies", x: canvas.width-100, y: canvas.height/2, width: 0, height: 16}]
+  const words = []
   renderer()
   document.addEventListener("keydown", keydownHandler)
   setInterval(wordsLogic, 10);
-  setInterval(createWords, 3000)
+  setInterval(createWords, 1000)
 
   function createWords(){
     words.push({word: "Squirrel", x: canvas.width-100, y: Math.floor(Math.random() * (canvas.height-16)) + 1, width: 0, height: 16})
@@ -23,24 +23,19 @@ function domLoadFunctions(){
 
   function wordLogic(word){
     word.x = word.x - 2
-    // if the front or back collides on x
-    const isAvatarLeftSideLefterThanLeftSideOfWord = avatar.x > word.x
-    const isAvatarLeftSideRighterThanRightSideOfWord = avatar.x < word.x + word.width
-    const isAvatarRightSideRighterThanLeftSideOfWord = avatar.x+avatar.width > word.x
-    const isAvatarRightSideLefterThanRightSideOfWord = avatar.x+avatar.width < word.x + word.width
-    const isAvatarBottomGreater
-    const areWeCollidingOnY = (avatar.y > word.y && avatar.y < word.y + word.height) ||
-    (avatar.y+avatar.height > word.y && avatar.y+avatar.height < word.y + word.height)  if (word.x<100){
-    console.log(word.x, avatar.x, areWeCollidingOnX)
-    console.log(word.y, avatar.y, areWeCollidingOnY)}
-    if (areWeCollidingOnX && areWeCollidingOnY){
-      alert("game over")
-      document.body.innerHTML = ""
-      avatar.x = 9999999
-      avatar.y = 9999999
-    }
+    // first condition: has the right side of the word hit zero? If so, you shouldn't get killed by it
+    // second condition: is any part of the word between 0 and the avatar width
+    const isRightSideOfWordGreaterThanZero = word.x + word.width > 0
+    const isAnyPartOfWordLessThanAvatarWidth = !!(((word.x < avatar.width) || (word.x + word.width <avatar.width)))
+    const isXColliding = !!(isRightSideOfWordGreaterThanZero && isAnyPartOfWordLessThanAvatarWidth)
+    console.log(isRightSideOfWordGreaterThanZero)
+    const isWordOnLineWithAvatarTop = (avatar.y < word.y + 3) && (avatar.y > (word.y - word.height -3))
+    const isWordOnLineWithAvatarBottom = (avatar.y+avatar.height < word.y + 3) && (avatar.y+avatar.height > (word.y - word.height -3))
+    const isWordOnLineWithAvatarMiddle = (avatar.y+(avatar.height/2) < word.y + 3) && (avatar.y+(avatar.height/2) > (word.y - word.height -3))
+    if (((isWordOnLineWithAvatarBottom||isWordOnLineWithAvatarMiddle)||isWordOnLineWithAvatarTop)&&isXColliding){
+      alert("Game Over!")
+        }
     if (word.x < -40){words.shift()}
-
   }
 
   function renderer(objs){
