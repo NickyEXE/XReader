1f062bd988451b0b2dca72c8e8cf91e668ea6b23
NickyEXE <dovern42@gmail.com>
2019-04-15 18:19:52 -0400

    pushing for the day

--- secret_laser_backend/app/controllers/essays_controller.rb
+++ secret_laser_backend/app/controllers/essays_controller.rb
@@ -1,6 +1,8 @@
 class EssaysController < ApplicationController
     def new
-        byebug
-        render :json => {"content": "hi"}
+      url = params["user_input"]
+      new_essay = User.find_or_create_by(username: params["username"]).essays.create(url: url)
+      @response = new_essay.get_content.to_s
+      render :json => {"response": @response}
     end
 end
--- secret_laser_backend/app/models/essay.rb
+++ secret_laser_backend/app/models/essay.rb
@@ -1,4 +1,17 @@
 class Essay < ApplicationRecord
     has_many :scores
     has_many :users, through: :scores
+
+  def get_content
+    uri = URI.parse("#{self.url}")
+    response = Net::HTTP.get_response(uri).body
+  end
+
+  def pull_p_tags
+    @doc = Nokogiri::XML(File.read(self.url))
+    byebug
+    @doc.xpath("//p")
+  end
+
+
 end
