b4e032cdd9c29005e3950ad4a5b517a6959a3bdf
NickyEXE <dovern42@gmail.com>
2019-04-15 18:35:52 -0400

    finished pulling first p tag

--- front-end/src/game.js
+++ front-end/src/game.js
@@ -1,4 +1,4 @@
-function startGame(){
+function startGame(essay){
   const canvasPlaceholder = document.getElementById("canvasPlaceholder")
   canvasPlaceholder.innerHTML = `<canvas id="myCanvas" width="200" height="100" style="border:1px solid #000000; background: url('https://ak2.picdn.net/shutterstock/videos/16719202/thumb/1.jpg')">
           <div id="dodger" style="bottom: 100px; left: 100px;"></div>
@@ -9,7 +9,7 @@ function startGame(){
   createTheBox()
   const avatar = {x: 0, y: Math.round(canvas.height/2), height: Math.round(canvas.height/15), width: Math.round(canvas.height/15)}
   const words = []
-  const warAndPeace = `“Well, Prince, so Genoa and Lucca are now just family estates of the Buonapartes. But I warn you, if you don’t tell me that this means war, if you still try to defend the infamies and horrors perpetrated by that Antichrist—I really believe he is Antichrist—I will have nothing more to do with you and you are no longer my friend, no longer my ‘faithful slave,’ as you call yourself! But how do you do? I see I have frightened you—sit down and tell me all the news.” It was in July, 1805, and the speaker was the well-known Anna Pávlovna Schérer, maid of honor and favorite of the Empress Márya Fëdorovna. With these words she greeted Prince Vasíli Kurágin, a man of high rank and importance, who was the first to arrive at her reception. Anna Pávlovna had had a cough for some days. She was, as she said, suffering from la grippe; grippe being then a new word in St. Petersburg, used only by the elite.`.split(" ")
+  const warAndPeace = essay.response[0].split(" ")
   let wordIterator = 0
   renderer()
   document.addEventListener("keydown", keydownHandler)
