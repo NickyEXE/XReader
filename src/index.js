6f9d80a1b302a2f8a9d43f0a9735a463bb6f5db2
tchang9 <tlchang@gmail.com>
2019-04-15 16:49:07 -0400

    WIP connecting front end and backend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
 
 document.addEventListener("DOMContentLoaded", domLoadFunctions)
+baseURL = `http://localhost:3000`
 
 function domLoadFunctions(){
   // startGame()
+  const urlButt = document.querySelector('#url-butt')
+  const form = document.querySelector('#form')
+
+  urlButt.addEventListener('click', processUrl)
+  form.addEventListener('click', fetchUrl)
+
 }
+
+function processUrl(e) {
+  // console.log(e.target)
+  const form = document.querySelector('#form')
+  form.innerHTML += 
+                `<div class="form-group" style=>
+                  <label>Url:</label>
+                  <input type="text" class="form-control" id="url" placeholder="Enter your Url">
+                </div>
+                <button id="urlBuffForm" type="submit" class="btn btn-primary">Play Secret Laser</button>
+                `
+}
+
+function fetchUrl(e) {
+  if (e.target.id === "urlBuffForm") {
+    e.preventDefault()
+    username = document.querySelector("#username").value
+    userInput = document.querySelector("#url").value
+
+    
+
+    fetch(`${baseURL}/new`,  {
+      method: 'POST',
+      mode: "no-cors",
+      body: JSON.stringify({username: username, user_input: userInput}),
+      headers:{
+      'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(json => startGame(json))
+  }
+}
+
+
