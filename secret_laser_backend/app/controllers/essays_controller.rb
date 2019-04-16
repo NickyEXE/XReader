b7856c1ef12a1fb03764c0f6367e476a2c818769
NickyEXE <dovern42@gmail.com>
2019-04-16 13:10:18 -0400

    active record holds on to page contents

--- secret_laser_backend/app/controllers/essays_controller.rb
+++ secret_laser_backend/app/controllers/essays_controller.rb
@@ -8,7 +8,7 @@ class EssaysController < ApplicationController
     def new
       url = params["user_input"]
       new_essay = User.find_or_create_by(username: params["username"]).essays.create(url: url)
-      @response = new_essay.parse_in_human
+      @response = new_essay.content
       render :json => {"response": @response}
     end
 end
