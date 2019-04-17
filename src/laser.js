c5d4d395943e11e1c81198d5f67bc22e8f2267a5
NickyEXE <dovern42@gmail.com>
2019-04-17 12:07:58 -0400

    laser now class on own page

---
+++ src/laser.js
@@ -0,0 +1,51 @@
+//needed classes
+// laser stuff
+class Laser {
+  constructor(x, y) {
+      this.x = x;
+      this.y = y;
+      this.constructor.lasersRendered.push(this)
+  }
+  behavior(){
+    // move the laser
+    this.x = this.x + this.constructor.speed
+    // delete the laser if it's off-screen
+    if (this.x > this.constructor.canvas.width+this.constructor.speed){
+      this.constructor.lasersRendered.shift()
+    }
+  }
+  render(){
+    this.constructor.ctx.fillStyle = 'yellow';
+    this.constructor.ctx.fillRect(this.x, this.y, this.constructor.speed, 1)
+  }
+  static changeLasers(){
+  this.lasersRendered.forEach(laser => laser.behavior())}
+
+  static laserSound(){
+    document.getElementById("laser").cloneNode(true).play();
+  }
+
+  static renderLasers(){
+    this.lasersRendered.forEach(laser => laser.render())
+  }
+
+  static initializeLaserVariables(canvas,avatar,ctx){
+    this.laserSet = true
+    this.speed = 5
+    this.lasersReady = 3
+    this.maxLasers = 3
+    this.lasersRendered = []
+    this.laserRefresh = 200
+    this.canvas = canvas
+    this.avatar = avatar
+    this.ctx = ctx
+  }
+
+  static shootLaser(){
+    if (Laser.laserSet){
+    Laser.laserSet = false
+    new Laser(Laser.avatar.x, (Laser.avatar.y +(Laser.avatar.height/2)))
+    Laser.laserSound()
+  }
+  }
+}
